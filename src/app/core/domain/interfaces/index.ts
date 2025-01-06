bbd136bf6f2abf8d73520cfa6ad901959cdf8a91
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-06 16:09:08 -0500

    add auth interfaces and organize existing domain interfaces

--- src/app/core/domain/interfaces/index.ts
+++ src/app/core/domain/interfaces/index.ts
@@ -1,17 +1,26 @@
+//*--------------------------------------------------------------------
+//* Interfaces
+//*--------------------------------------------------------------------
+
+//? User Interfaces
 export * from './user/create-user.interface';
 export * from './user/update-user.interface';
 export * from './user/user.interface';
 
+//? List Interfaces
 export * from './list/create-list.interface';
 export * from './list/update-list.interface';
 export * from './list/list.interface';
 
+//? Task Interfaces
 export * from './task/create-task.interface';
 export * from './task/update-task.interface';
 export * from './task/task.interface';
 
+//? Subtask Interfaces
 export * from './subtask/create-subtask.interface';
 export * from './subtask/update-subtask.interface';
 export * from './subtask/subtask.interface';
 
+//? Auth Interfaces
 export * from './auth/login.interface';
