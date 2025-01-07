cfe6e815d366958380ff79be53c9162e46ac96ff
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-07 16:13:54 -0500

    add LoginPageComponent and SignupPageComponent; update routing for authentication module

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthLayoutComponent } from '../../presentation/layouts/auth-layout/auth-layout.component';
 import { AuthRoutingModule } from '../routes/auth-routing.module';
+import { LoginPageComponent } from '../../presentation/pages/login-page/login-page.component';
+import { SignupPageComponent } from '../../presentation/pages/signup-page/signup-page.component';
 
 
 
 
 @NgModule({
   declarations: [
-    AuthLayoutComponent
+    AuthLayoutComponent,
+
+    LoginPageComponent,
+    SignupPageComponent
   ],
   imports: [
     CommonModule,
--- src/app/modules/routes/auth-routing.module.ts
+++ src/app/modules/routes/auth-routing.module.ts
@@ -1,17 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthLayoutComponent } from '../../presentation/layouts/auth-layout/auth-layout.component';
-
+import { LoginPageComponent } from '../../presentation/pages/login-page/login-page.component';
+import { SignupPageComponent } from '../../presentation/pages/signup-page/signup-page.component';
 
 const routes: Routes = [
-  { path: '',
+  {
+    path: '',
     component: AuthLayoutComponent,
-    children: []
-  }
+    children: [
+      {
+        path: 'login',
+        title: 'Iniciar Sesión',
+        component: LoginPageComponent,
+      },
+      {
+        path: 'sign-up',
+        title: 'Registro',
+        component: SignupPageComponent,
+      },
+      {
+        path: '',
+        redirectTo:'login',
+        pathMatch: 'full'
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
