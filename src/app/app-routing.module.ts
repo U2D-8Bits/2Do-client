35da20871adbd0309c30d6ea9c63d1db022d9706
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-07 15:52:27 -0500

    add routing for authentication and dashboard modules; include HttpClientModule in app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//*---------------------------------------------------------
+//* Routes
+//*---------------------------------------------------------
+const routes: Routes = [
+  {
+    path: '', loadChildren: () => import('./modules/routes/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'dashboard', loadChildren: () => import('./modules/routes/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path:'',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
+
+//*---------------------------------------------------------
+//* NgModule
+//*---------------------------------------------------------
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
