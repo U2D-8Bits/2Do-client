f7113b5d25d875c049f602c3616db68a3d92afe1
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-07 15:50:55 -0500

    add AuthModule and AuthRoutingModule for authentication layout

---
+++ src/app/modules/routes/auth/auth-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from '../../../presentation/layouts/auth-layout/auth-layout.component';
+
+const routes: Routes = [
+  { path: '',
+    component: AuthLayoutComponent,
+    children: []
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
