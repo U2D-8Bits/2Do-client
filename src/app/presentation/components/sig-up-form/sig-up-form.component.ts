fdbcdffeebbdf15f48a1dab1fe371c2c71e6ee28
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-07 22:19:49 -0500

    update SignUpFormComponent to improve form structure and rename selector; integrate into SignupPageComponent

--- src/app/presentation/components/sig-up-form/sig-up-form.component.ts
+++ src/app/presentation/components/sig-up-form/sig-up-form.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-sig-up-form',
+  selector: 'app-sign-up-form',
   templateUrl: './sig-up-form.component.html',
   styleUrl: './sig-up-form.component.css'
 })
