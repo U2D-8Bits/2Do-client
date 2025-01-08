b42a881147490b721817c41c721f7ada3b5252cb
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-07 21:40:07 -0500

    add barrel files for components and pages

---
+++ src/app/presentation/pages/index.ts
@@ -0,0 +1,7 @@
+//*---------------------------------------------------------
+//* Barrel File for Pages
+//*---------------------------------------------------------
+
+export {ErrorPageComponent} from './error-page/error-page.component';
+export {LoginPageComponent} from './login-page/login-page.component';
+export {SignupPageComponent} from './signup-page/signup-page.component';
