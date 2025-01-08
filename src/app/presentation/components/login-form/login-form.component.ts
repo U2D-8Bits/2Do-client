03bb09c04a90337ed06c7f84790274f9d2f66d55
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-08 15:46:01 -0500

    feat: implement reactive form for login with validation and loading state

--- src/app/presentation/components/login-form/login-form.component.ts
+++ src/app/presentation/components/login-form/login-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { UiModule } from "../../../modules/ui/ui.module";
+import { UiModule } from '../../../modules/ui/ui.module';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -7,10 +8,30 @@ import { UiModule } from "../../../modules/ui/ui.module";
   styleUrl: './login-form.component.css',
 })
 export class LoginFormComponent {
+  //*-----------------------------------------
+  //* Constructor
+  //*-----------------------------------------
+  constructor(private fb: FormBuilder) {}
 
+  //*-----------------------------------------
+  //* Properties
+  //*-----------------------------------------
 
-  onClick(){
-    console.log('Login button clicked');
+  loginForm = this.fb.group({
+    str_user_email: ['', [Validators.required, Validators.email]],
+    str_user_password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  loading: boolean = false;
+
+  //? Submit Form - Login
+  onSubmit(): void {
+    if( this.loginForm.valid ){
+      this.loading = true;
+      setTimeout(() =>{
+        console.log('Formulario enviado', this.loginForm.value);
+        this.loading = false;
+      },2500)
+    }
   }
-
 }
