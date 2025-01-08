ecc8db3b30c2f44d5d9b340a22353c80f2f95b2c
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-07 22:08:38 -0500

    refactor InputComponent to enhance template structure and add input properties

--- src/app/presentation/ui/input/input.component.ts
+++ src/app/presentation/ui/input/input.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-input',
+  selector: 'app-ui-input',
   templateUrl: './input.component.html',
   styleUrl: './input.component.css'
 })
 export class InputComponent {
+
+  //*---------------------------------------------------------
+  //* Input Properties
+  //*---------------------------------------------------------
+  @Input() type: string = 'text';
+  @Input() placeholder: string = '';
+  @Input() label: string = '';
+  @Input() value: string = '';
+  @Input() required: boolean = false;
+  @Input() disabled: boolean = false;
 
 }
