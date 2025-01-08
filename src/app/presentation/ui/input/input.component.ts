76db8057edb69c3264304c8772b035f68a4e7008
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-08 15:45:56 -0500

    feat: implement ControlValueAccessor for InputComponent and enhance input handling

--- src/app/presentation/ui/input/input.component.ts
+++ src/app/presentation/ui/input/input.component.ts
@@ -1,11 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-ui-input',
   templateUrl: './input.component.html',
-  styleUrl: './input.component.css'
+  styleUrl: './input.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent ),
+      multi: true
+    }
+  ]
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
 
   //*---------------------------------------------------------
   //* Input Properties
@@ -13,8 +21,41 @@ export class InputComponent {
   @Input() type: string = 'text';
   @Input() placeholder: string = '';
   @Input() label: string = '';
-  @Input() value: string = '';
   @Input() required: boolean = false;
   @Input() disabled: boolean = false;
+
+  value:string = '';
+
+  //*---------------------------------------------------------
+  //* ControlValueAccessor Implementation
+  //*---------------------------------------------------------
+
+  //? Callbacks
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  //? Methods
+
+  writeValue(value: string): void {
+    this.value = value || '';
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  onInput(event: Event): void {
+    const inputValue = (event.target as HTMLInputElement)?.value || '';
+    this.value = inputValue;
+    this.onChange(inputValue);
+    this.onTouched();
+  }
 
 }
