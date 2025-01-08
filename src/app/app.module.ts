3aee395087c133f6516f54bf389d6e0797f2c64d
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-07 21:47:36 -0500

    add InputComponent with basic template and structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ErrorPageComponent } from './presentation/pages/error-page/error-page.component';
+import { InputComponent } from './presentation/ui/input/input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ErrorPageComponent,
+    InputComponent,
   ],
   imports: [
     BrowserModule,
