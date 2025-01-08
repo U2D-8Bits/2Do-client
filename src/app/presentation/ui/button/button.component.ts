ecf6390b3ac2f488b337303b8f92f9eb86dcbb55
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-08 14:07:47 -0500

    refactor ButtonComponent to use a button element with input properties and click event emitter

--- src/app/presentation/ui/button/button.component.ts
+++ src/app/presentation/ui/button/button.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class ButtonComponent {
 
+  //*------------------------------------------
+  //* Button Properties
+  //*------------------------------------------
+
+  //? Button Text
+  @Input()label: string = '';
+
+  //? Button Type
+  @Input()type: string = 'button';
+
+  //? Button Disabled
+  @Input()disabled: boolean = false;
+
+  //? Button Click Event
+  @Output() onClick = new EventEmitter<any>();
+
+
+  //*------------------------------------------
+  //* Button Methods
+  //*------------------------------------------
+  //? On Button Click Event
+  onClickEvent(){
+    this.onClick.emit();
+  }
 }
