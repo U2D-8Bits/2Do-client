6125494efc6548d5e7fe092dda965232d8e56194
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-09 19:27:42 -0500

    feat: implement ToastComponent with customizable message, type, and auto-dismiss functionality

--- src/app/presentation/ui/toast/toast.component.ts
+++ src/app/presentation/ui/toast/toast.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-toast',
   templateUrl: './toast.component.html',
   styleUrl: './toast.component.css'
 })
-export class ToastComponent {
+export class ToastComponent implements OnInit {
+
+  //*----------------------------------------------------
+  //* Constructor
+  //*----------------------------------------------------
+  constructor(){}
+
+  //*----------------------------------------------------
+  //* Properties
+  //*----------------------------------------------------
+
+  @Input() type: 'success' | 'error' | 'warning' = 'success';
+  @Input() message: string = '';
+  @Input() duration: number = 3000;
+
+  isVisible: boolean = true;
+  private timeOut: any;
+
+  //*----------------------------------------------------
+  //* Methods
+  //*----------------------------------------------------
+
+  closeToast(): void {
+    this.isVisible = false;
+    if( this.timeOut){
+      clearTimeout(this.timeOut);
+    }
+  }
+
+  //*----------------------------------------------------
+  //* ngOnInit
+  //*----------------------------------------------------
+  ngOnInit(): void {
+    if( this.duration > 0 ){
+      this.timeOut = setTimeout(() => this.closeToast(), this.duration);
+    }
+  }
 
 }
