6e590757465cbb9ce64d18539768ea39f6115b87
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-11 18:58:41 -0500

    feat: enhance AuthService with token verification and authentication status checks

--- src/app/application/services/auth/auth.service.ts
+++ src/app/application/services/auth/auth.service.ts
@@ -4,30 +4,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
-import { LoginInterface, LoginResponseInterface, UserInterface } from '../../../core/domain/interfaces';
-import { catchError, map, Observable } from 'rxjs';
+import {
+  LoginInterface,
+  LoginResponseInterface,
+  UserInterface,
+} from '../../../core/domain/interfaces';
+import { BehaviorSubject, catchError, map, Observable } from 'rxjs';
 import { StorageService } from '../storage.service';
+import { AuthStatusEnum } from '../../../core/domain/enum/auth-status.enum';
 
 //*----------------------------------------
 //* Injectable
 //*----------------------------------------
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 
 //*----------------------------------------
 //* Service
 //*----------------------------------------
 export class AuthService {
-
   //*----------------------------------------
   //* Constructor
   //*----------------------------------------
   constructor(
     private httpClient: HttpClient,
     private storageService: StorageService
-  ){}
-
+  ) {}
 
   //*----------------------------------------
   //* Properties
@@ -35,31 +38,54 @@ export class AuthService {
 
   private readonly baseUrl: string = environment.baseUrl;
 
+  authStatus$ = new BehaviorSubject<AuthStatusEnum>(AuthStatusEnum.checking);
+
   //*----------------------------------------
   //* Methods
   //*----------------------------------------
 
   //? Method to save auth info
-  private saveData(user: UserInterface, token: string): boolean{
+  private saveData(user: UserInterface, token: string): boolean {
     this.storageService.setItem('token', token);
     return true;
   }
 
   //? Login Method
-  login(loginInterface: LoginInterface): Observable<boolean>{
-
+  login(loginInterface: LoginInterface): Observable<boolean> {
     const url = `${this.baseUrl}/auth/login`;
 
-    return this.httpClient.post<LoginResponseInterface>(url, loginInterface)
-    .pipe(
-      map( ({user, token}) =>{
-        this.saveData(user, token);
-        return true;
-      }),
-      catchError( error => {
-        return new Observable<boolean>(error)
-      })
-    )
+    return this.httpClient
+      .post<LoginResponseInterface>(url, loginInterface)
+      .pipe(
+        map(({ user, token }) => {
+          this.saveData(user, token);
+          return true;
+        }),
+        catchError((error) => {
+          return new Observable<boolean>(error);
+        })
+      );
   }
 
+  //? Verify Token Method
+  verifyToken(token: string): boolean {
+    try {
+      const payload = JSON.parse(atob(token.split('.')[1]));
+      const isExpired = payload.exp * 1000 < Date.now();
+      return !isExpired;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  //? Verify Auth Method
+  checkAuthStatus(): void {
+    const token = this.storageService.getItem('token');
+
+    if (token && this.verifyToken(token)) {
+      this.authStatus$.next(AuthStatusEnum.authenticated);
+    } else {
+      this.authStatus$.next(AuthStatusEnum.notAuthenticated);
+    }
+  }
 }
