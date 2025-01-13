ea26f5d0bfd214a174adce5596b7b518b3991907
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-13 16:35:54 -0500

    feat: add AsideComponent to DashboardModule and export it

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,17 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardLayoutComponent } from '../../presentation/layouts/dashboard-layout/dashboard-layout.component';
 import { DashboardRoutingModule } from '../routes/dashboard-routing.module';
+import { AsideComponent } from '../../presentation/components';
 
 
 
 
 @NgModule({
   declarations: [
-    DashboardLayoutComponent
+    DashboardLayoutComponent,
+
+    //* Components
+    AsideComponent
   ],
   imports: [
     CommonModule,
     DashboardRoutingModule
+  ],
+  exports: [
+
+    //* Components
+    AsideComponent
   ]
 })
 export class DashboardModule { }
