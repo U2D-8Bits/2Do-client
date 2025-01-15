e14e895f684081a01e56f50c12aabeeb73d275de
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-14 22:54:09 -0500

    feat: export NavbarComponent from components index

--- src/app/presentation/components/index.ts
+++ src/app/presentation/components/index.ts
@@ -6,3 +6,4 @@ export { LoginFormComponent } from './login-form/login-form.component';
 export { SigUpFormComponent } from './sig-up-form/sig-up-form.component';
 export { ResetPasswordFormComponent } from './reset-password-form/reset-password-form.component';
 export { AsideComponent } from './aside/aside.component';
+export { NavbarComponent } from './navbar/navbar.component';
