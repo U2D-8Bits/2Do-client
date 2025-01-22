62b82a677d855b9987d1aa3ab95cf4c34d868d23
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-22 18:14:17 -0500

    feat: enhance User entity with optional profile picture and image URL retrieval

--- src/app/core/domain/entities/user.entity.ts
+++ src/app/core/domain/entities/user.entity.ts
@@ -2,8 +2,15 @@
 //* Imports
 //*-------------------------------------------------------------------
 
-import { List } from "./list.entity";
-import { Task } from "./task.entity";
+import { List } from './list.entity';
+import { Task } from './task.entity';
+import { environment } from '../../../../environments/environment';
+
+//*-------------------------------------------------------------------
+//* Constants
+//*-------------------------------------------------------------------
+
+const baseUrl = environment.baseUrl;
 
 //*-------------------------------------------------------------------
 //* Class Definition
@@ -11,14 +18,26 @@ import { Task } from "./task.entity";
 
 export class User {
   constructor(
-    int_user_id: number,
-    str_user_username: string,
-    str_user_email: string,
-    str_user_password: string,
-    str_user_password_confirm: string,
-    str_user_profile_picture: string,
-    dt_user_created_at: Date,
-    lists: List[],
-    tasks: Task[]
-  ){}
+    public int_user_id: number,
+    public str_user_username: string,
+    public str_user_email: string,
+    public str_user_password: string,
+    public str_user_password_confirm: string,
+    public dt_user_created_at: Date,
+    public str_user_profile_picture?: string,
+    public lists?: List[],
+    public tasks?: Task[]
+  ) {}
+
+  getImageUrl() {
+    if (!this.str_user_profile_picture) {
+      return `${baseUrl}/upload/default-profile.png`;
+    } else if (this.str_user_profile_picture.includes('https')) {
+      return this.str_user_profile_picture;
+    } else if (this.str_user_profile_picture) {
+      return `${baseUrl}/upload/profile_pictures/${this.str_user_profile_picture}`;
+    }else{
+      return `${baseUrl}/upload/default-profile.png`;
+    }
+  }
 }
