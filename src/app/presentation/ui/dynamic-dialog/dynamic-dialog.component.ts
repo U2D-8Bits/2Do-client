085052f9598974c44ddea302061a1a17220238ce
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-24 18:43:31 -0500

    feat: add DynamicDialogComponent with initial template and styles

---
+++ src/app/presentation/ui/dynamic-dialog/dynamic-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-dialog',
+  templateUrl: './dynamic-dialog.component.html',
+  styleUrl: './dynamic-dialog.component.css'
+})
+export class DynamicDialogComponent {
+
+}
