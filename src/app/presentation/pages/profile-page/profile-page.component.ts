ed6337d3e365d68b0182c95b3839210f1c410c80
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-24 17:35:38 -0500

    feat: implement user data retrieval in ProfilePageComponent

--- src/app/presentation/pages/profile-page/profile-page.component.ts
+++ src/app/presentation/pages/profile-page/profile-page.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+//***********************************************************************/
+//* Imports
+//***********************************************************************/
+import { Component, OnInit } from '@angular/core';
+import { StorageService, UserService } from '../../../application/services';
+import { UserInterface } from '../../../core/domain/interfaces';
 
+//***********************************************************************/
+//* @Component
+//***********************************************************************/
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.css'
 })
-export class ProfilePageComponent {
+
+//***********************************************************************/
+//* ProfilePageComponent
+//***********************************************************************/
+export class ProfilePageComponent implements OnInit {
+
+  //***********************************************************************/
+  //* Constructor
+  //***********************************************************************/
+  constructor(
+    private userService: UserService,
+    private storageService: StorageService,
+  ) { }
+
+  //***********************************************************************/
+  //* ngOnInit
+  //***********************************************************************/
+  ngOnInit(): void {
+
+    const userID: number | null = Number(this.storageService.getSessionItem('ID'));
+
+    this.userService.getUserDataByID(userID).subscribe({
+      next: (response: UserInterface) => {
+        this.userData = response;
+        console.log(this.userData);
+      },
+      error: (error: any) => {
+        console.error(error);
+      }
+    })
+  }
+
+  //***********************************************************************/
+  //* Properties
+  //***********************************************************************/
+  userData: UserInterface | null = null;
+
+  //***********************************************************************/
+  //* Methods
+  //***********************************************************************/
 
 }
