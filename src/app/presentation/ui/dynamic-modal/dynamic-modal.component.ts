d56ee65332b6d12316d27f6f743e3e2d0c622c60
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-26 20:44:30 -0500

    feat: add DynamicModalComponent with basic template and structure

---
+++ src/app/presentation/ui/dynamic-modal/dynamic-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-modal',
+  templateUrl: './dynamic-modal.component.html',
+  styleUrl: './dynamic-modal.component.css'
+})
+export class DynamicModalComponent {
+
+}
