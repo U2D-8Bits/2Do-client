f44bb3fdf6789a57c168b176965a07bab78a9120
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-26 20:45:40 -0500

    feat: export DynamicModalComponent in UI index for accessibility

--- src/app/presentation/ui/index.ts
+++ src/app/presentation/ui/index.ts
@@ -6,3 +6,4 @@ export { InputComponent } from './input/input.component';
 export { ButtonComponent } from './button/button.component';
 export { ToastComponent } from './toast/toast.component';
 export { ToastContainerComponent } from './toast-container/toast-container.component';
+export { DynamicModalComponent } from './dynamic-modal/dynamic-modal.component';
