1b2fb7576ad422bffcf2aa69982023c641774654
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-26 20:40:37 -0500

    feat: remove DynamicDialogComponent export from the UI module

--- src/app/presentation/ui/index.ts
+++ src/app/presentation/ui/index.ts
@@ -6,4 +6,3 @@ export { InputComponent } from './input/input.component';
 export { ButtonComponent } from './button/button.component';
 export { ToastComponent } from './toast/toast.component';
 export { ToastContainerComponent } from './toast-container/toast-container.component';
-export { DynamicDialogComponent } from './dynamic-dialog/dynamic-dialog.component';
