d0ebc54c2ed9e814444663161585d3cd03ccf80d
Pablo Israel Bolaños Párraga <u2d.8bits@gmail.com>
2025-01-26 20:06:38 -0500

    feat: enhance DynamicDialogComponent with input properties and event emitters

--- src/app/presentation/ui/dynamic-dialog/dynamic-dialog.component.ts
+++ src/app/presentation/ui/dynamic-dialog/dynamic-dialog.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+//************************************************************/
+//* Imports
+//************************************************************/
+import { Component, Input, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
+//************************************************************/
+//* Component
+//************************************************************/
 @Component({
   selector: 'app-dynamic-dialog',
   templateUrl: './dynamic-dialog.component.html',
   styleUrl: './dynamic-dialog.component.css'
 })
+
+//************************************************************/
+//* Class
+//************************************************************/
 export class DynamicDialogComponent {
+
+  //************************************************************/
+  //* Properties
+  //************************************************************/
+
+  @Input() title: string = '';
+  @Input() content: string | null = null;
+  @Input() isVisible: boolean = false;
+  @Input() confirmText: string = 'Confirmar';
+  @Input() cancelText: string = 'Cancelar';
+
+  @Output() onConfirm = new EventEmitter<void>();
+  @Output() onCancel = new EventEmitter<void>();
+
+  //************************************************************/
+  //* Methods
+  //************************************************************/
+
+  //? Método para confirmar
+  handleConfirm(): void {
+    this.onConfirm.emit();
+  }
+
+  //? Método para cancelar
+  handleCancel(): void {
+    this.onCancel.emit();
+  }
+
 
 }
